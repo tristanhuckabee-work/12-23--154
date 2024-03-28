17f54d80435bda2f206c8cbcbf25cbd3dc372906
tristan <thuckabee.work@gmail.com>
2024-03-28 18:16:55 -0500

    finished 01

--- 01-static-assets/server.js
+++ 01-static-assets/server.js
@@ -2,7 +2,27 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Your code here 
+  const { method, url } = req;
+  console.log(method, url);
+
+  if (method == 'GET' && url.endsWith('.css') || url.endsWith('.jpg')) {
+    let asset = fs.readFileSync(`./assets${url.split('/static')[1]}`);
+    res.statusCode = 200;
+
+    if (url.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+    if (url.endsWith('.jpg')) {
+      res.setHeader('Content-Type', 'image/jpeg');
+    }
+    
+    return res.end(asset);
+  }
+
+  const html = fs.readFileSync('./index.html', 'utf-8');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  return res.end(html);
 });
 
 const port = 5000;
